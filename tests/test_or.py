d574a636c16c62831e4c5a720a023a66ad707ac3
Tejtex <raxeli1.mc@gmail.com>
2025-06-19 16:37:20 +0200

    a

--- tests/test_or.py
+++ tests/test_or.py
@@ -1,17 +1,24 @@
 from revex.parser import Parser
-from revex.generators import StringGenerator, GroupGenerator, OrGenerator, RepeatGenerator
+from revex.generators import (
+    StringGenerator,
+    GroupGenerator,
+    OrGenerator,
+    RepeatGenerator,
+)
 import pytest
 import re
+
 
 def test_or_generator():
     gen = OrGenerator(StringGenerator("a"), StringGenerator("b"))
     assert gen.generate({}) in ["a", "b"]
 
+
 def test_or_parser():
     parser = Parser()
-    tests = ['a|b', 'ab|c', 'a|b|c']
+    tests = ["a|b", "ab|c", "a|b|c"]
 
     for test in tests:
         for i in range(100):
             gen = parser.parse(test)
-            assert re.match(f'^{test}$', gen.generate({'max_repeats': 3}))
+            assert re.match(f"^{test}$", gen.generate({"max_repeats": 3}))
--- tests/test_string.py
+++ tests/test_string.py
@@ -1,7 +1,20 @@
 from revex.parser import Parser
-from revex.generators import StringGenerator, GroupGenerator, OrGenerator, RepeatGenerator
+from revex.generators import (
+    StringGenerator,
+    GroupGenerator,
+    OrGenerator,
+    RepeatGenerator,
+)
 import pytest
+
 
 def test_string_generator():
     gen = StringGenerator("hello")
+    assert gen.generate({}) == "hello"
+
+
+def test_string_parser():
+    parser = Parser()
+    gen = parser.parse("hello")
+    assert isinstance(gen, StringGenerator)
     assert gen.generate({}) == "hello"
