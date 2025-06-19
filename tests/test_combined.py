0ab74e964965982c51d8061229b694d12ab8c65b
Tejtex <raxeli1.mc@gmail.com>
2025-06-19 16:36:31 +0200

    a

--- tests/test_combined.py
+++ tests/test_combined.py
@@ -1,13 +1,19 @@
 from revex.parser import Parser
-from revex.generators import StringGenerator, GroupGenerator, OrGenerator, RepeatGenerator
+from revex.generators import (
+    StringGenerator,
+    GroupGenerator,
+    OrGenerator,
+    RepeatGenerator,
+)
 import pytest
 import re
 
+
 def test_parser():
     parser = Parser()
-    tests = []
+    tests = ["(a|b)*c+", "a|(b(c|a)?)", "(a+b*c?d|e)+", "x?y*z+", "a(b|c)*d+"]
 
     for test in tests:
         for i in range(100):
             gen = parser.parse(test)
-            assert  re.match(f'^{test}$', gen.generate({'max_repeats': 3}))
+            assert re.match(f"^{test}$", gen.generate({"max_repeats": 3}))
